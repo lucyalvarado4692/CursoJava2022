abe920315d9ffe379120dac8d8221d1dd4766517
Alvarado Rodríguez <lucy-elena.alvarado-rodriguez@capgemini.com>
2022-11-22 17:07:14 +0100

    ejercicios hoy

---
+++ ej4/src/com/capgemini/training/java/ej2/MiPila.java
@@ -0,0 +1,24 @@
+package com.capgemini.training.java.ej2;
+
+public class MiPila {
+	// arreglo arryalist
+	// operaciones pop, push, peek, reverse
+	int indice = 0;
+	char arrayList[];
+	arrayList = new char[10];
+	
+	
+	public static void main(String[] args) {
+		
+		
+	
+	}
+	
+	public char push(int e) {
+		return arrayList.push(e);
+	}
+	
+	
+
+	
+}

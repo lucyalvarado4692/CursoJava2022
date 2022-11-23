47a62d05b201df28df30ebce5ba1673d1f5cbc2e
Alvarado Rodríguez <lucy-elena.alvarado-rodriguez@capgemini.com>
2022-11-23 08:32:33 +0100

    Ejercicio 2

--- ej2/src/ej2/HelloWorld.java
+++ ej2/src/ej2/HelloWorld.java
@@ -1,5 +1,20 @@
 package ej2;
 
 public class HelloWorld {
+	public static void main(String[] args) {
+		// imprimir Hello world
+		System.out.println("Hello world");
+		//tablas de multiplicar
+		int index = 0, result = 0, tabla = 0;
+		for (tabla = 1; tabla <= 10; tabla++) {
+			for(index = 1; index <10; index++) {
+				result = tabla * index;
+				System.out.println(tabla + "x" + index + "=" + result);
+			}
+			result = tabla * index;
+			System.out.println(tabla + "x" + index + "=" + result);
+			System.out.println("");
+		}
+	}
 
 }
